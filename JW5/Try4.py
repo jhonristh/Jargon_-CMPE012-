278af9fb9bf05c11dc91462ce466c2341b4b0f29
jhonristh <jhonreyoquendo01@gmail.com>
2025-10-11 12:37:52 +0800

    Week 5

---
+++ JW5/Try4.py
@@ -0,0 +1,35 @@
+import math
+
+#Numbers Numbers
+myIntA = 14
+myIntB = 17
+myIntC = 2025
+myFloatA = 1.545
+myFloatB = 67.55
+myFloatC = 6565.656
+myComplexA = 14 - 7j
+myComplexB = 9 - 11j
+myComplexC = 9898 - 9j
+# +, -, /, *
+mySumA = myIntA + myIntB + myIntC
+print(mySumA)
+myDiff = myIntA - myIntB - myIntC
+print(myDiff)
+myProd = myIntA * myIntB * myIntC
+print(myProd)
+myQuo =  myIntA / myIntB * myIntC
+print(myQuo)
+print(round(myQuo))
+#modulo
+remainder = myIntA % myIntB
+print(remainder)
+
+
+#complex calculations
+iAdd = myComplexA + myComplexB
+print(iAdd)
+
+#after math alib addition
+Angle = math.cos(1)
+print(Angle)
+print(radia(Angle))
---
+++ JW5/Try6.py
@@ -0,0 +1,15 @@
+##membership operators
+#in, not in, is, is not
+isIn = (14 in [34,43,343,33,23,14,35,98])
+print(isIn)
+isIn = (14 in [34,43,343,33,23,35,98])
+print(isIn)
+IsInW = ("Baby" in "Nice one, Baby") #true
+print(IsInW)
+IsInW = ("Baby" in "Nice one, Bruh") #false
+print(IsInW)
+##IS Is Is
+isIs = ("hiraya" is "astig") #False
+print(isIs)
+isIS = ("hiraya" is "hiraya") #True
+print(isIS)
